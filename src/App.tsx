1fd1fb2ccb9fe63c7e572d7ef37e625c6ad17c72
DecidedCard <heajun35@gmail.com>
2024-03-06 11:39:46 +0900

    [feat] Header 파일 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import Header from "./components/Header";
 import Input from "./components/Input";
 import TodoList from "./components/TodoList";
 
 const App = () => {
   return (
     <>
+      <Header />
       <Input />
       <TodoList checkDone={false} />
       <TodoList checkDone={true} />
